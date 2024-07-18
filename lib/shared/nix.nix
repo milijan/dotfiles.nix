c21b23d79dcdab9b02aba395e251652b8e6239a6
milijan <milijan@users.noreply.github.com>
2024-07-18 04:37:43 +0200

    Update nix.nix trusted user

--- lib/shared/nix.nix
+++ lib/shared/nix.nix
@@ -9,7 +9,7 @@
     trusted-public-keys = [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
     ];
-    trusted-users = ["erikreinert"];
+    trusted-users = ["milijan"];
     warn-dirty = false;
   };
 }
